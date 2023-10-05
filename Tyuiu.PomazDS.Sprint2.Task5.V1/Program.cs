63e5d4c98490784af885b34d121e4ac128d8a808
admin <zed-n.v@mail.ru>
2023-10-06 00:09:27 +0500

    Task5

---
+++ Tyuiu.PomazDS.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PomazDS.Sprint2.Task5.V1.Lib;
+using AutoFill_lib;
+
+namespace Tyuiu.PomazDS.Sprint2.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            AutoFill ptrn = new AutoFill();
+            ptrn.MainPattern("Оператор switch", 5, 1, "Написать программу, которая использует оператор switch вычисляет требуемое значение и возвращает результат. Условие: По данному номеру месяца, определите количество дней в этом месяце.");
+
+            Console.WriteLine("Введите номер месяца: ");
+            int value = int.Parse(Console.ReadLine());
+
+            ptrn.ResultPattern();
+
+            Console.WriteLine(ds.FindMonthDaysCount(value));
+
+            Console.ReadKey();
+        }
+    }
+}
