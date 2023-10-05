de68515f5dabda23042265d86c9828f93d27e0bb
admin <zed-n.v@mail.ru>
2023-10-05 17:40:23 +0500

    Task3

--- Tyuiu.PomazDS.Sprint2.Task3.V10.Test/DataServiceTest.cs
+++ Tyuiu.PomazDS.Sprint2.Task3.V10.Test/DataServiceTest.cs
@@ -9,8 +9,33 @@ namespace Tyuiu.PomazDS.Sprint2.Task3.V10.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCondition()
         {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(6.333, res);
+        }
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(-1, res);
+        }
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(0.25, res);
+        }
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -16;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(-175.938, res);
         }
     }
 }
