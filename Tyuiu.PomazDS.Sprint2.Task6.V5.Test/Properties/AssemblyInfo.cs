661eb2dbefcd3c7e1de1d49f06c05c9b311e8832
admin <zed-n.v@mail.ru>
2023-10-08 15:59:25 +0500

    Task6

---
+++ Tyuiu.PomazDS.Sprint2.Task6.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PomazDS.Sprint2.Task6.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PomazDS.Sprint2.Task6.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1bcf7d5-1552-417b-88a8-ddceba82d2aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PomazDS.Sprint2.Task6.V5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PomazDS.Sprint2.Task6.V5.Lib;
+using AutoFill_lib;
+
+namespace Tyuiu.PomazDS.Sprint2.Task6.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            AutoFill ptrn = new AutoFill();
+            ptrn.MainPattern("Получение результата из switch",
+                6,
+                5,
+                "Написать программу, которая использует сокращенную форму записи оператора switch вычисляет требуемое значение и возвращает результат. Условие: Игральным картам условно присвоены следующие порядковые номера в зависимости от их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14. Порядковые номера остальных карт соответствуют их названиям («шестерка», «девятка» и т. п.). По заданному номеру карты k (6 <=k <= 14) определить достоинство соответствующей карты.");
+
+            Console.WriteLine("Введите номер карты: ");
+            int value = int.Parse(Console.ReadLine());
+
+            ptrn.ResultPattern();
+
+            Console.WriteLine(ds.FindCardValue(value));
+
+            Console.ReadKey();
+
+        }
+    }
+}
